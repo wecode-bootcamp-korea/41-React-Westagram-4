0e83c0e1d1a85fb0ae33c9ba4b75c58ef9518a91
김기윤 <ki@Kis-MacBook-Pro.local>
2022-12-21 17:19:13 +0900

    ADD: Navigation 추가

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,50 @@
 import React from "react";
+import "./Nav.scss";
 
 function Nav() {
-  return <h1>This is Nav</h1>;
+  return (
+    <nav>
+      <div className="topmenu">
+        <div className="toplogo">
+          <div>
+            <i className="fa fa-instagram" />
+          </div>
+          <div>
+            <span className="logoword">&nbsp;Westagram</span>
+          </div>
+        </div>
+        <div className="nav-center">
+          <div className="searchbar">
+            <i className="fa fa-search" />
+            <input id="search_id" type="text" placeholder="검색" />
+          </div>
+          <div className="suggestion-box">
+            <ul className="search-results" />
+          </div>
+        </div>
+        <div className="topright">
+          <div>
+            <img
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
+              alt="search"
+            />
+          </div>
+          <div>
+            <img
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+              alt="likes"
+            />
+          </div>
+          <div>
+            <img
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
+              alt="profile"
+            />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
