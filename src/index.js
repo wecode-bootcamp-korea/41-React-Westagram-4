b215d15757c4dd8f2567c6366806a79c8d560ce1
조윤환 <114235846+jyh2610@users.noreply.github.com>
2022-12-21 16:06:46 +0900

    modify:두번째 수정 완료

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Router from "./Routers";
-import "./styles/common.scss";
+import Router from "./Router";
 import "./styles/reset.scss";
+import "./styles/common.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Router />);
