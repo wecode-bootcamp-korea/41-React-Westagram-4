4a2bcf7863d55351692f7fff7f5c382182476539
조윤환 <114235846+jyh2610@users.noreply.github.com>
2022-12-21 15:59:28 +0900

    modify:수정사항완료

--- src/Router.js
+++ src/Router.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Nav from './components/Nav/Nav';
+import Nav from "./components/Nav/Nav";
 // 김코드의 컴포넌트
-import Loginchoyunhwan from './pages/choyunhwan/Login/Login';
-import Mainchoyunhwan from './pages/choyunhwan/Main/Main';
+import Loginchoyunhwan from "./pages/choyunhwan/Login/Login";
+import Mainchoyunhwan from "./pages/choyunhwan/Main/Main";
 
-import Loginkimkiyoon from './pages/kiyoonkim/Login/Login';
-import Mainkimyoon from './pages/kiyoonkim/Main/Main';
+import Loginkimkiyoon from "./pages/kiyoonkim/Login/Login";
+import Mainkimyoon from "./pages/kiyoonkim/Main/Main";
 
-import Loginkimjinwoo from './pages/jinwookim/Login/Login';
-import Mainjinwookim from './pages/jinwookim/Main/Main';
+import Loginkimjinwoo from "./pages/jinwookim/Login/Login";
+import Mainjinwookim from "./pages/jinwookim/Main/Main";
 
 // 김개발의 컴포넌트
-import Loginleeseyoon from './pages/seyoon/Login/Login';
-import Mainleeseyoon from './pages/seyoon/Main/Main';
+import Loginleeseyoon from "./pages/seyoon/Login/Login";
+import Mainleeseyoon from "./pages/seyoon/Main/Main";
 
 // import 한 컴포넌트에 대한 경로를 각각 설정해줍니다.
 const Router = () => {
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 function Nav() {
   return <h1>This is Nav</h1>;
