b726802be34bdf9426a939408a37fef57669f2d2
조윤환 <114235846+jyh2610@users.noreply.github.com>
2022-12-22 17:36:58 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/choyunhwan/Login/Login.js
+++ src/pages/choyunhwan/Login/Login.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Loginkimjinwoo() {
-  return <h1>this is login</h1>;
+import "./Login.scss";
+import { useNavigate } from "react-router-dom";
+function Login() {
+  const navigate = useNavigate();
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  const saveUserId = event => {
+    setId(event.target.value);
+  };
+  const saveUserPw = event => {
+    setPw(event.target.value);
+  };
+  console.log(id);
+  console.log("pw", pw);
+  return (
+    <section className="loginpage">
+      <div className="loginContainer">
+        <header id="logo">Westagram</header>
+
+        <input
+          className="idinput"
+          type="text"
+          onChange={saveUserId}
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+        />
+
+        <input
+          type="password"
+          className="passwordinput"
+          placeholder="비밀번호"
+          onChange={saveUserPw}
+        />
+
+        <button
+          className="loginbtn"
+          onClick={() => {
+            navigate("/Main");
+          }}
+        >
+          로그인
+        </button>
+
+        <p className="findpassword">비밀번호를 잊으셨나요?</p>
+      </div>
+    </section>
+  );
 }
 
-export default Loginkimjinwoo;
+export default Login;
--- src/pages/choyunhwan/Main/Main.js
+++ src/pages/choyunhwan/Main/Main.js
@@ -1,7 +1,203 @@
-import React from "react";
+import React, { Components } from "react";
 
-function Mainchoyunhwan() {
-  return <h1>This is Main</h1>;
+import "./Main.scss";
+
+function Main() {
+  return (
+    <>
+      <nav className="menubar">
+        <div className="logo">
+          <img className="westalogo" alt="profile" src="images/instagram.png" />
+          <p className="westa">westagram</p>
+        </div>
+        <input className="searchbar" type="text" placeholder="검색" />
+        <div className="navicon">
+          <img
+            alt="img"
+            src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
+          />
+          <img
+            alt="img"
+            src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+          />
+          <img
+            alt="img"
+            src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png
+      "
+          />
+        </div>
+      </nav>
+      <main>
+        <div className="feeds">
+          <article className="articleborder">
+            <div className="articleTop">
+              <img className="profile" alt="profile" src="./images/tae.jpeg" />
+              <p>LBJ</p>
+              <img className="dot" alt="profile" src="./images/more.png" />
+            </div>
+            <div className="articleCenter">
+              <img
+                className="feedimg"
+                alt="profile"
+                src="./images/r662876_2_1296x729_16-9.jpg"
+              />
+              <div className="imoticon">
+                <img
+                  id="heartIcon"
+                  className="iconSize"
+                  alt="profile"
+                  src="./images/emtyheart.png"
+                />
+                <img
+                  id="ICONSIZE"
+                  className="iconSize"
+                  alt="profile"
+                  src="./images/chat.png"
+                />
+                <img
+                  id="ICONSIZE"
+                  className="iconSize"
+                  alt="profile"
+                  src="./images/12027312981650100392-128.png"
+                />
+                <img
+                  id="ICONSIZE"
+                  className="iconSize"
+                  alt="profile"
+                  src="./images/save-instagram.png"
+                />
+              </div>
+              <div className="commentwindow">
+                <div className="feedLikeNum">
+                  <img
+                    className="feedcomentimg"
+                    alt="profile"
+                    src="./images/kim.jpeg"
+                  />
+                  <span className="feedLikeNumfont">
+                    LBJ님 외 10명이 좋아합니다.
+                  </span>
+                </div>
+                <div className="LIST">
+                  <ul className="commentList" />
+                </div>
+              </div>
+              <div className="comment">
+                <input
+                  className="commentinput"
+                  type="text"
+                  placeholder="댓글 달기..."
+                />
+                <button id="commentInput" className="commentUD unstyled-button">
+                  게시
+                </button>
+              </div>
+            </div>
+          </article>
+        </div>
+        <div className="right">
+          <div className="main-right">
+            <div className="myprofile">
+              <img
+                className="profile"
+                alt="profile"
+                src="https://post-phinf.pstatic.net/MjAyMDA4MjdfNjQg/MDAxNTk4NTAwODI3MTI5.Oiiy_qjRWUkDb2SndjMb8YsKh7x9vQ5V8b4WxM_E23og.KjTZW-1KnkB43PBsS9qMjD1PXf984NR65jPLmo5jHvcg.JPEG/shutterstock_713670574.jpg?type=w1200"
+              />
+              <p className="name">ChoYunHwan</p>
+            </div>
+          </div>
+
+          <div className="story">
+            <div className="storyMenu">
+              <span className="storyFontMenu sfMenu">스토리</span>
+              <span className="storyFontMenu sfMenu2">모두 보기</span>
+            </div>
+
+            <div className="IDTIME">
+              <div className="story_ID">
+                <img
+                  className="story_profile"
+                  alt="profile"
+                  src="./images/tae.jpeg"
+                />
+                <div className="storyFontContainer">
+                  <span className="story-font">태연</span>
+                  <span className="story-font fontlightgray">2분전</span>
+                </div>
+                <div className="story_ID">
+                  <img
+                    className="story_profile"
+                    alt="profile"
+                    src="./images/다운로드 (1).jpeg"
+                  />
+                  <div className="storyFontContainer">
+                    <span className="story-font">durant</span>
+                    <span className="story-font fontlightgray">2분전</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="recomand">
+              <div className="storyMenu">
+                <span className="storyFontMenu reMenu">회원님을위한추천</span>
+                <span className="storyFontMenu reMenu2">모두보기</span>
+              </div>
+              <div className="story_ID">
+                <img
+                  className="story_profile"
+                  alt="profile"
+                  src="./images/curry.jpeg"
+                />
+                <div className="storyFontContainer">
+                  <span className="story-font">curry</span>
+                  <span className="story-font fontlightgray">
+                    yun외 10명이 알고있습니다.
+                  </span>
+                  <span className="fallow">팔로우</span>
+                </div>
+                <div className="story_ID">
+                  <img
+                    className="story_profile"
+                    alt="profile"
+                    src="./images/다운로드.jpeg"
+                  />
+                  <div className="storyFontContainer">
+                    <span className="story-font">durant</span>
+                    <span className="story-font fontlightgray">
+                      yun외 10명이 알고있습니다.
+                    </span>
+
+                    <span className="fallow">팔로우</span>
+                  </div>
+                  <div className="story_ID">
+                    <img
+                      className="story_profile"
+                      alt="profile"
+                      src="./images/다운로드.jpeg"
+                    />
+                    <div className="storyFontContainer">
+                      <span className="story-font ">durant</span>
+                      <span className="story-font fontlightgray">
+                        yun외 10명이 알고있습니다.
+                      </span>
+                      <span className="fallow">팔로우</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="explain fontlightgray">
+              <span>
+                westagram 정보·지원·홍보·
+                센터·API·채용정보·개인정보처리방침·약관·디렉터리·프로필·해시태그·언어
+              </span>
+              <p className="WESTAGRAM">@2022 WESTAGRAM</p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 
-export default Mainchoyunhwan;
+export default Main;
