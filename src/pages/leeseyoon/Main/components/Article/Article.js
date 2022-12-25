4acc88f02397e10c9080ec310e0370f0b7f1a5dd
Seyoon <seyoonleee@gmail.com>
2022-12-25 23:19:22 +0900

    Refactor: Article 컴포넌트화

---
+++ src/pages/leeseyoon/Main/components/Article/Article.js
@@ -0,0 +1,76 @@
+import "./Article.scss";
+
+const Article = () => {
+  return (
+    <article>
+      <div className="feed-header">
+        <div className="left">
+          <div className="new-recommended-img-box">
+            <img
+              className="new-story-img"
+              src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg"
+              alt=""
+            />
+          </div>
+          <div className="font-weight-600">Valentin</div>
+        </div>
+        <div className="right">
+          <i className="fa-solid fa-ellipsis" />
+        </div>
+      </div>
+      <div className="feed-img">
+        <img
+          src="https://images.unsplash.com/photo-1621508654686-809f23efdabc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGhvcml6b258ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+          alt=""
+        />
+      </div>
+
+      <div className="section-wrap font-size-14">
+        <section className="feed-body">
+          <div className="left">
+            <i className="fa-regular fa-heart" />
+            <i className="fa-regular fa-comment" />
+            <i className="fa-regular fa-share-from-square" />
+          </div>
+          <div className="right">
+            <i className="fa-regular fa-bookmark" />
+          </div>
+        </section>
+
+        <div className="like-wrap">
+          <div className="like-poeple-img-box">
+            <img
+              className="new-story-img"
+              src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1038.jpg"
+              alt=""
+            />
+          </div>
+          <strong>Ronaldo</strong>
+          <span>님&nbsp</span>
+          <strong>외 6명이</strong>
+          <span>이 좋아합니다.</span>
+        </div>
+
+        <div className="text">
+          Temporibus et tenetur ut tenetur quam corrupti possimus.
+        </div>
+
+        <div className="comments">
+          <div className="comment">
+            <strong className="">Sydni </strong>
+            <span>Cool!!✨</span>
+          </div>
+        </div>
+        <form onsubmit="return false;" action="" className="new-comment">
+          <i className="fa-regular fa-face-smile" />
+          <input type="text" placeholder="댓글 달기..." />
+          <button className="posting font-blue font-weight-600 postingDeactivity">
+            게시
+          </button>
+        </form>
+      </div>
+    </article>
+  );
+};
+
+export default Article;
