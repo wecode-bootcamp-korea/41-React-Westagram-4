103e5a5d2c3b57538cdd6884c2d7e996318c0cd3
Seyoon <seyoonleee@gmail.com>
2022-12-25 15:48:00 +0900

    Add: Mission1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/leeseyoon/Login/Login.js
+++ src/pages/leeseyoon/Login/Login.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Login.scss";
 
 const Login = () => {
   const navigate = useNavigate();
   const goToMain = () => navigate("/Mainleeseyoon");
+
+  const [userId, setUserId] = useState("");
+  const [userPw, setUserPw] = useState("");
+
+  const saveUserId = e => setUserId(e.target.value);
+  const saveUserPw = e => setUserPw(e.target.value);
 
   return (
     <div className="login">
@@ -17,8 +23,16 @@ const Login = () => {
             className="user-id"
             type="text"
             placeholder="전화번호, 사용자 이름 또는 이메일"
+            value={userId}
+            onChange={saveUserId}
           />
-          <input className="user-pw" type="password" placeholder="비밀번호" />
+          <input
+            className="user-pw"
+            type="password"
+            placeholder="비밀번호"
+            value={userPw}
+            onChange={saveUserPw}
+          />
           <button className="login-btn btn-light-blue" onClick={goToMain}>
             로그인
           </button>
--- src/pages/leeseyoon/Main/Main.js
+++ src/pages/leeseyoon/Main/Main.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Main.scss";
-// import { feedData } from "../../../public/data/data";
 
 const Main = () => {
   return (
