67809e202bd42001eca1eeedf6f06bbd049b35cd
조윤환 <114235846+jyh2610@users.noreply.github.com>
2022-12-25 12:19:39 +0900

    Add : Mission 4 - 댓글 컴포넌트화, Props로 데이터 전달 구현

--- src/pages/choyunhwan/Main/Main.js
+++ src/pages/choyunhwan/Main/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Nav from "../Components/Nav";
+import Nav from "../../../components/Nav/Nav";
 import "./Main.scss";
 import Feeds from "../Components/Feeds";
 import Right from "../Components/Right";
