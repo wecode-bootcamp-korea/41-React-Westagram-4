8cdbe65ad26cee52bfa26bdd92eedfde92422be8
wecaners <wecaners@naver.com>
2022-12-26 22:53:15 +0900

    Refactor: 상수데이터 파일 분리

--- src/pages/jinwookim/Main/Main.js
+++ src/pages/jinwookim/Main/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import Nav from "../../../components/Nav/Nav";
 import "./Main.scss";
 import Feeds from "./Feeds";
+import { ASIDE_LIST } from "./AsideList";
 
 function Mainkimjinwoo() {
   return (
@@ -146,16 +147,3 @@ const Recommend = () => {
 };
 
 export default Mainkimjinwoo;
-const ASIDE_LIST = [
-  { id: 1, asideName: "소개∙" },
-  { id: 2, asideName: "도움말∙" },
-  { id: 3, asideName: "홍보 센터∙" },
-  { id: 4, asideName: "API∙" },
-  { id: 5, asideName: "채용 정보∙" },
-  { id: 6, asideName: "개인정보처리방침∙" },
-  { id: 7, asideName: "약관∙" },
-  { id: 8, asideName: "위치∙" },
-  { id: 9, asideName: "인기 계정∙" },
-  { id: 10, asideName: "해시태그∙" },
-  { id: 11, asideName: "언어∙" },
-];
