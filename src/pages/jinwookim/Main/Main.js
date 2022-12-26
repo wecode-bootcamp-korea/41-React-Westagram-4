2221124d3991e525abd2c8e3d52201c99555d21a
wecaners <wecaners@naver.com>
2022-12-26 22:51:08 +0900

    Refactor: Aside 하단부분 상수데이터로 변경

--- src/pages/jinwookim/Main/Main.js
+++ src/pages/jinwookim/Main/Main.js
@@ -14,8 +14,11 @@ function Mainkimjinwoo() {
           <Story />
           <Recommend />
           <p>
-            Instagram 정보 . 지원 . 홍보 센터 . API . 채용 정보 .
-            개인정보처리방침 . 약관 . 디렉터리 . 프로필 . 해시태그 . 언어
+            {/* Instagram 정보 . 지원 . 홍보 센터 . API . 채용 정보 .
+            개인정보처리방침 . 약관 . 디렉터리 . 프로필 . 해시태그 . 언어 */}
+            {ASIDE_LIST.map(menuList => {
+              return <span key={menuList.id}>{menuList.asideName}</span>;
+            })}
           </p>
           <p>© 2019 INSTAGRAM</p>
         </div>
@@ -143,3 +146,16 @@ const Recommend = () => {
 };
 
 export default Mainkimjinwoo;
+const ASIDE_LIST = [
+  { id: 1, asideName: "소개∙" },
+  { id: 2, asideName: "도움말∙" },
+  { id: 3, asideName: "홍보 센터∙" },
+  { id: 4, asideName: "API∙" },
+  { id: 5, asideName: "채용 정보∙" },
+  { id: 6, asideName: "개인정보처리방침∙" },
+  { id: 7, asideName: "약관∙" },
+  { id: 8, asideName: "위치∙" },
+  { id: 9, asideName: "인기 계정∙" },
+  { id: 10, asideName: "해시태그∙" },
+  { id: 11, asideName: "언어∙" },
+];
