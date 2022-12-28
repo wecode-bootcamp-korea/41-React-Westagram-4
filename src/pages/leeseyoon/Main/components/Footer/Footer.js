9e84b2d474b9950d5e22fe69fe891f637d9ffebc
Seyoon <seyoonleee@gmail.com>
2022-12-28 15:54:22 +0900

    Add: 상수데이터 파일 분리 및 Aside에서 상수데이터 활용

--- src/pages/leeseyoon/Main/components/Footer/Footer.js
+++ src/pages/leeseyoon/Main/components/Footer/Footer.js
@@ -1,53 +1,21 @@
 import "./Footer.scss";
+import { FOOTER_INFO_LIST } from "./data";
+console.log(FOOTER_INFO_LIST);
 
 const Footer = () => {
   return (
     <footer className="footer-wrap">
       <ul>
-        <li>
-          <a href="">Instagram 정보</a>
-          {" · "}
-        </li>
-        <li>
-          <a href="">지원</a>
-          {" · "}
-        </li>
-        <li>
-          <a href="">홍보 센터</a>
-          {" · "}
-        </li>
-        <li>
-          <a href="">API</a>
-          {" · "}
-        </li>
-        <li>
-          <a href="">채용 정보</a>
-          {" · "}
-        </li>
-        <li>
-          <a href="">개인정보처리방침</a>
-          {" · "}
-        </li>
-        <li>
-          <a href="">약관</a>
-          {" · "}
-        </li>
-        <li>
-          <a href="">디렉터리</a>
-          {" · "}
-        </li>
-        <li>
-          <a href="">프로필</a>
-          {" · "}
-        </li>
-        <li>
-          <a href="">해시태그</a>
-          {" · "}
-        </li>
-        <li>
-          <a href="">언어</a>
-          {" · "}
-        </li>
+        {FOOTER_INFO_LIST.map((info, idx) => {
+          return (
+            <li key={info.id}>
+              <a>
+                {idx !== 0 ? " · " : ""}
+                {info.text}
+              </a>
+            </li>
+          );
+        })}
       </ul>
       <div>2022 INSTAGRAM</div>
     </footer>
---
+++ src/pages/leeseyoon/Main/components/Footer/data.js
@@ -0,0 +1,13 @@
+export const FOOTER_INFO_LIST = [
+  { id: 1, link: "", text: "Instagram 정보" },
+  { id: 2, link: "", text: "지원" },
+  { id: 3, link: "", text: "홍보 센터" },
+  { id: 4, link: "", text: "API" },
+  { id: 5, link: "", text: "채용 정보" },
+  { id: 6, link: "", text: "개인정보처리방침" },
+  { id: 7, link: "", text: "약관" },
+  { id: 8, link: "", text: "디렉터리" },
+  { id: 9, link: "", text: "프로필" },
+  { id: 10, link: "", text: "해시태그" },
+  { id: 11, link: "", text: "언어" },
+];
