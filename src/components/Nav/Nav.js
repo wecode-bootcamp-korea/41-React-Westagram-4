30b8f0988f5827e15fc67eb1404f71a632428fc5
김기윤 <ki@Kis-MacBook-Pro.local>
2022-12-29 13:05:04 +0900

    Modify: 공통Nav로 수정

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -16,25 +16,13 @@ function Nav() {
         <div className="nav-center">
           <div className="searchbar">
             <i className="fa fa-search" />
-            <input type="text" placeholder="검색" />
+            <input id="search_id" type="text" placeholder="검색" />
           </div>
           <div className="suggestion-box">
             <ul className="search-results" />
           </div>
         </div>
         <div className="topright">
-          <div>
-            <img
-              src="https://static.thenounproject.com/png/682477-200.png"
-              alt="home"
-            />
-          </div>
-          <div>
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/5883/5883507.png"
-              alt="directmessage"
-            />
-          </div>
           <div>
             <img
               src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
@@ -47,7 +35,7 @@ function Nav() {
               alt="likes"
             />
           </div>
-          <div className="my-profile">
+          <div>
             <img
               src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
               alt="profile"
